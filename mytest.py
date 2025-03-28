646356f380360a3e5456586628dee27e86e7e312
BOINGBINGBONK <154535173+Evicencio-05@users.noreply.github.com>
2025-03-28 05:54:42 -0700

    Added UPDATE statement. Need to check for buggos.

---
+++ mytest.py
@@ -0,0 +1,19 @@
+import SimpleDB
+
+db = SimpleDB.SimpleDB("db.json")
+
+# db.create_table("users", ["id", "name", "age", "word"])
+
+# db.insert("users", {"id": "3", "name": "Bill", "age": 4})
+# db.insert("users", {"id": "1", "name": "B", "age": 125, "word": "test"})
+# db.insert("users", {"id": "2", "name": "Bi", "age": 23, "word": "test"})
+# db.insert("users", {"id": "4", "name": "Bil", "age": 74, "word": "test"})
+# db.insert("users", {"id": "5", "name": "Billl", "age": 14, "word": "test"})
+
+
+# db.update("users", {"age": 4, "word": "test"}, where = {"age": {"gt": 25}})
+
+# print(db.execute("SELECT name, age FROM users WHERE age > 25"))
+# print(db.execute("INSERT INTO users (id, name, age) VALUES (3, Charlie, 22)"))
+# print(db.execute("DELETE FROM users"))
+print(db.execute("UPDATE users SET name = 'BERRR', word = 'TEST', age = 300 WHERE age > 100"))
--- parser.py
+++ parser.py
@@ -1,5 +1,16 @@
 import re
 
+def get_where(parts: list[any], query: dict) -> None:
+    if "WHERE" in parts:
+            where_idx = parts.index('WHERE')
+            col, op, val = parts[where_idx + 1], parts[where_idx + 2], int(parts[where_idx + 3])
+            op_map = {
+                '>': "gt",
+                '=': "eq",
+                '<': "lt"
+                }
+            query["where"][col] = {op_map[op]: val}
+
 def parse_query(query_str):
     parts = re.findall(r'\w+|[^\w\s,]', query_str)
     query = {}
@@ -11,15 +22,7 @@ def parse_query(query_str):
         columns_str = query_str.split('SELECT')[1].split('FROM')[0].strip()
         query["columns"] = [col.strip() for col in columns_str.split(',')]
         query["table"] = parts[cols_end + 1]
-        if "WHERE" in parts:
-            where_idx = parts.index('WHERE')
-            col, op, val = parts[where_idx + 1], parts[where_idx + 2], int(parts[where_idx + 3])
-            op_map = {
-                '>': "gt",
-                '=': "eq",
-                '<': "lt"
-                }
-            query["where"][col] = {op_map[op]: val}
+        get_where(parts, query)
     elif parts[0].upper() == "INSERT" and parts[1].upper() == "INTO":
         query["type"] = "INSERT INTO"
         
@@ -44,16 +47,34 @@ def parse_query(query_str):
         table_name: str = parts[parts.index("FROM") + 1]
         query["table"] = table_name
     
-        if "WHERE" in parts:
-            where_idx = parts.index('WHERE')
-            col, op, val = parts[where_idx + 1], parts[where_idx + 2], int(parts[where_idx + 3])
-            op_map = {
-                '>': "gt",
-                '=': "eq",
-                '<': "lt"
-                }
-            query["where"][col] = {op_map[op]: val}    
-        else:
-            query["where"] = None
+        get_where(parts, query)
+    elif parts[0].upper() == "UPDATE":
+        query["type"] = "UPDATE"
+        query["where"] = {}
+        
+        table_name: str = parts[parts.index("UPDATE") + 1]
+        query["table"] = table_name
+        
+        get_where(parts, query)
+        
+        updated_values_parts = query_str.split('SET')[1].split("WHERE")[0].strip()
+        updated_values_parts = updated_values_parts.split(',')
+        
+        values: dict = {}
+        for part in updated_values_parts:
+            key, value = part.split('=')
+            key = key.strip()
+            value = value.strip()
+            
+            value = int(value) if value.isdigit() else value
+            
+            if isinstance(value, str):
+                if value.startswith("'") and value.endswith("'"):
+                    value = value.strip("'")
+
+                
+            values[key] = value
+            
+        query["values"] = values
             
     return query
